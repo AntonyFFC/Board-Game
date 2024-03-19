dc635af6e9c8fa55bb84b841899306dd182dfe0c
AntonyFFC <antekffc@gmail.com>
2024-03-19 20:35:40 +0100

    improved table class

--- Table.cpp
+++ Table.cpp
@@ -11,3 +11,29 @@ Table::~Table()
 void Table::draw()
 {
 }
+
+bool Table::tableClicked(sf::Vector2i mousePosition)
+{
+    int clickMinY = minY + cellHeight;
+    if (mousePosition.x >= minX && mousePosition.x <= maxX && mousePosition.y >= clickMinY && mousePosition.y <= maxY)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Table::createTexture()
+{
+}
+
+void Table::drawHeaders()
+{
+}
+
+void Table::setUpDimensions()
+{
+    minX = position.x;
+    minY = position.y;
+    maxX = position.x+sumOfCellWidths;
+    maxY = minY + cellHeight * numberOfItems;
+}
--- Table.h
+++ Table.h
@@ -12,13 +12,20 @@ public:
 	~Table();
 
 	void draw();
+	bool tableClicked(sf::Vector2i mousePosition);
 
 private:
+	void createTexture();
+	void drawHeaders();
+	void setUpDimensions();
+
+	sf::RenderWindow* target;
 	sf::RectangleShape cell;
 	sf::Text text;
 	std::map<std::string, sf::Sprite> iconSprites;
 	std::vector<sf::Texture*> iconTextures;
 	std::string headers[8];
+	sf::Vector2f position;
 	int minX;
 	int minY;
 	int maxX;
@@ -26,7 +33,7 @@ private:
 	int cellWidths[8];
 	int sumOfCellWidths;
 	int cellHeight;
-	float gap;
+	int numberOfItems;
 	sf::RenderTexture tableRenderTexture;
 	sf::Sprite tableSprite;
 };
