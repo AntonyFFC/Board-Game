166cfe9e83a63c6b66ce1445ca669ce62ec2f5bd
AntonyFFC <antekffc@gmail.com>
2025-06-17 19:11:31 +0200

    Implemented a new class EquipmentPile for the dropping equipment on a pile.

---
+++ EquipmentPile.cpp
@@ -0,0 +1,72 @@
+#include "EquipmentPile.h"
+
+EquipmentPile::EquipmentPile(std::tuple<int, int, int> hexCoords)
+	: hexCoords(hexCoords), scaleFactor(0.05f), rotationAngle(0.0f), xPos(0.0f), yPos(0.0f)
+{
+
+    loadTexture();
+    sprite.setTexture(texture);
+    setUpPosition();
+}
+
+EquipmentPile::~EquipmentPile()
+{
+    // Cleanup if necessary
+}
+
+void EquipmentPile::loadTexture()
+{
+    if (!texture.loadFromFile("assets/equipment/equipment pile.png"))
+    {
+        throw std::runtime_error("Failed to load equipment pile texture");
+    }
+}
+
+void EquipmentPile::addEquipment(Equipment* equipment)
+{
+    equipmentList.push_back(equipment);
+}
+
+void EquipmentPile::removeEquipment(Equipment* equipment)
+{
+    auto it = std::find(equipmentList.begin(), equipmentList.end(), equipment);
+    if (it != equipmentList.end())
+    {
+        equipmentList.erase(it);
+    }
+}
+
+const std::vector<Equipment*>& EquipmentPile::getEquipment() const
+{
+    return equipmentList;
+}
+
+void EquipmentPile::setPosition(float x, float y)
+{
+    xPos = x;
+    yPos = y;
+    setUpPosition();
+}
+
+void EquipmentPile::setUpPosition()
+{
+    sprite.setScale(scaleFactor, scaleFactor);
+    sprite.setPosition(xPos, yPos);
+    sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+    sprite.setRotation(rotationAngle);
+}
+
+sf::Vector2f EquipmentPile::getPosition() const
+{
+    return sf::Vector2f(xPos, yPos);
+}
+
+std::tuple<int, int, int> EquipmentPile::getHexCoords() const
+{
+    return hexCoords;
+}
+
+void EquipmentPile::draw(sf::RenderWindow& window)
+{
+    window.draw(sprite);
+}
---
+++ EquipmentPile.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "Equipment.h"
+class EquipmentPile
+{
+public:
+    EquipmentPile(std::tuple<int, int, int> hexCoords);
+    ~EquipmentPile();
+
+    void addEquipment(Equipment* equipment);
+    void removeEquipment(Equipment* equipment);
+    const std::vector<Equipment*>& getEquipment() const;
+    void setPosition(float x, float y);
+    sf::Vector2f getPosition() const;
+    std::tuple<int, int, int> getHexCoords() const;
+    void draw(sf::RenderWindow& window);
+
+private:
+    void loadTexture();
+    void setUpPosition();
+
+    std::vector<Equipment*> equipmentList;
+    sf::Sprite sprite;
+    sf::Texture texture;
+    float xPos;
+    float yPos;
+    std::tuple<int, int, int> hexCoords;
+    float scaleFactor;
+    float rotationAngle;
+};
+
